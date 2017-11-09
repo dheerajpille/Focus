ee68e9cf57d92c5e51015e02420587cd5081750e
dheerajpille <dheerajpille@gmail.com>
2017-11-09 10:55:51 -0500

    Implemented saving and retrieving sites to browser storage

--- js/sites.js
+++ js/sites.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    $('body').on('click', 'button', function(){
-        var sites = $('textarea')[0].value;
-        chrome.storage.sync.set({'sites': sites}, function() {
-            alert(sites);
-        });
+    chrome.storage.local.get('sites', function(result){
+        var values = result.sites;
+        $('textarea').val(values);
+    });
+    $('button').on('click',function(){
+        var sites = $('textarea')[0].value.replace(/\s+/g,' ').replace(/>(\s)</g,'>\n<');
+        chrome.storage.local.set({'sites': sites});
+        location.reload();
     });
 });
