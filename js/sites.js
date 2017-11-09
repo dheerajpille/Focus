95488c37794cadb24ee0c948ee7c4bce3747542d
dheerajpille <dheerajpille@gmail.com>
2017-11-09 14:08:41 -0500

    Added case for no websites being blocked

--- js/sites.js
+++ js/sites.js
@@ -1,22 +1,27 @@
 $(document).ready(function(){
     if ($('textarea').get(0)) {
         chrome.storage.local.get('sites', function(result){
-            var values = result.sites.split(/\s+/).join('\n');
+            var values = result.sites.join('\n');
             $('textarea').val(values);
         });
         $('button').on('click',function(){
-            var sites = $('textarea')[0].value.replace(/\s+/g,' ').replace(/>(\s)</g,'>\n<');
+            var sites = $('textarea')[0].value.replace(/\s+/g,' ').replace(/>(\s)</g,'>\n<').split(/\s+/);
             chrome.storage.local.set({'sites': sites});
-            location.reload();
         });
     } else if ($('div.col-xs-6 ul').get(0)) {
         chrome.storage.local.get('sites', function(result){
-            var values = result.sites.split(/\s+/);
-            values.forEach(function(index){
+            var values = result.sites;
+            if (values.length === 0 || values[0] === "") {
                 $('div.col-xs-6 ul').append(
-                    '<li class="list-group-item" style="display: block; word-break: break-all;">'+index+'</li>'
+                    '<li class="list-group-item" style="display: block; word-break: break-all;">No websites blocked</li>'
                 );
-            });
+            } else {
+                values.forEach(function(index){
+                    $('div.col-xs-6 ul').append(
+                        '<li class="list-group-item" style="display: block; word-break: break-all;">'+index+'</li>'
+                    );
+                });
+            }
         });
     }
 });
