c4871102d5fc4edc23b20161dc6849631312d804
dheerajpille <dheerajpille@gmail.com>
2017-11-09 12:07:05 -0500

    Implemented blocked sites list in settings index

--- js/sites.js
+++ js/sites.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-    chrome.storage.local.get('sites', function(result){
-        var values = result.sites;
-        $('textarea').val(values.split(/\s+/).join('\n'));
-    });
-    $('button').on('click',function(){
-        var sites = $('textarea')[0].value.replace(/\s+/g,' ').replace(/>(\s)</g,'>\n<');
-        chrome.storage.local.set({'sites': sites});
-        location.reload();
-    });
+    if ($('textarea').get(0)) {
+        chrome.storage.local.get('sites', function(result){
+            var values = result.sites.split(/\s+/).join('\n');
+            $('textarea').val(values);
+        });
+        $('button').on('click',function(){
+            var sites = $('textarea')[0].value.replace(/\s+/g,' ').replace(/>(\s)</g,'>\n<');
+            chrome.storage.local.set({'sites': sites});
+            location.reload();
+        });
+    } else if ($('div.col-xs-5 ul').get(0)) {
+        chrome.storage.local.get('sites', function(result){
+            var values = result.sites.split(/\s+/);
+            values.forEach(function(index){
+                $('div.col-xs-5 ul').append(
+                    '<li class="list-group-item visible-lg-inline-block">'+index+'</li>'
+                );
+            });
+        });
+    }
 });
