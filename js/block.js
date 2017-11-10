d908c311e60f60a9c3aa4ef74a7a4f31375cc245
dheerajpille <dheerajpille@gmail.com>
2017-11-10 15:01:48 -0500

    Reduced redirect time from blocked sites

--- js/block.js
+++ js/block.js
@@ -2,22 +2,20 @@ document.documentElement.style.visibility='hidden';
 
 $(document).ready(function() {
     var site = window.location.href.toLowerCase();
-    chrome.storage.local.get('sites', function (result) {
+    chrome.storage.local.get(['sites', 'state'], function (result) {
         var sites = result.sites;
+        var state = result.state;
         var block = false;
-        chrome.storage.local.get('state', function(result) {
-            var state = result.state;
-            if (state) {
-                if (sites !== undefined) {
-                    sites.forEach(function (index) {
-                        if (index !== "" && site.indexOf(index) > -1) {
-                            block = true;
-                            $(location).attr('href', chrome.runtime.getURL('html/block.html'));
-                        }
-                    });
-                }
+        if (state) {
+            if (sites !== undefined) {
+                sites.forEach(function (index) {
+                    if (index !== "" && site.indexOf(index) > -1) {
+                        block = true;
+                        $(location).attr('href', chrome.runtime.getURL('html/block.html'));
+                    }
+                });
             }
-        });
+        }
         if (!block) {
             document.documentElement.style.visibility = 'visible';
         }
