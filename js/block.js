3b26bd684900bdc6ea169d5b711f32125734349b
dheerajpille <dheerajpille@gmail.com>
2017-11-10 10:11:54 -0500

    Removed small time frame where user can view blocked site

--- js/block.js
+++ js/block.js
@@ -1,24 +1,17 @@
+document.documentElement.style.visibility='hidden';
+
 $(document).ready(function(){
     var site = window.location.href;
     chrome.storage.local.get('sites', function(result) {
         var values = result.sites;
+        var block = false;
         values.forEach(function (index) {
-            if (index !== "" && site.indexOf(index) !== -1) {
-                $('head').empty();
-                $('head').append('<title>Blocked sites</title>');
-                $('head').append(
-                    '<link rel="stylesheet" ' +
-                    'href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">'
-                );
-                $('body').empty();
-                $('body').append(
-                    '<br>' +
-                    '<div class="text-center">' +
-                    '<i class="glyphicon glyphicon-eye-close" style="font-size: 10em;"></i>' +
-                    '<h4>This website is blocked.</h4>' +
-                    '</div>'
-                );
+            if (index !== "" && site.indexOf(index) > -1) {
+                block = true;
             }
         });
+        if (!block) {
+            document.documentElement.style.visibility='';
+        }
     });
 });
