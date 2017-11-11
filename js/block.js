26dfb3cf977513d331f1e328e683acc8ae515882
dheerajpille <dheerajpille@gmail.com>
2017-11-11 13:37:29 -0500

    Fixed blocking site before page load

--- js/block.js
+++ js/block.js
@@ -1,23 +1,20 @@
 document.documentElement.style.visibility='hidden';
-
-$(document).ready(function() {
-    var site = window.location.href.toLowerCase();
-    chrome.storage.local.get(['sites', 'state'], function (result) {
-        var sites = result.sites;
-        var state = result.state;
-        var block = false;
-        if (state) {
-            if (sites !== undefined) {
-                sites.forEach(function (index) {
-                    if (index !== "" && site.indexOf(index) > -1) {
-                        block = true;
-                        $(location).attr('href', chrome.runtime.getURL('html/block.html'));
-                    }
-                });
-            }
+var site = window.location.href.toLowerCase();
+chrome.storage.local.get(['sites', 'state'], function (result) {
+    var sites = result.sites;
+    var state = result.state;
+    var block = false;
+    if (state) {
+        if (sites !== undefined) {
+            sites.forEach(function (index) {
+                if (index !== "" && site.indexOf(index) > -1) {
+                    block = true;
+                    $(location).attr('href', chrome.runtime.getURL('html/block.html'));
+                }
+            });
         }
-        if (!block) {
-            document.documentElement.style.visibility = 'visible';
-        }
-    });
+    }
+    if (!block) {
+        document.documentElement.style.visibility = 'visible';
+    }
 });
